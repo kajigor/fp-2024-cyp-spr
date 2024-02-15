7f315e0c88746a54678882816ae98b6a4756aa50
GrigoriiSolnyshkin <grisolnyshkin04@gmail.com>
2024-02-15 19:23:32 +0200

    style refactor

--- Main.hs
+++ Main.hs
@@ -7,15 +7,15 @@ ageOn :: String -> Float -> Float
 ageOn planet ageInSeconds = ageInSeconds / (yearToEarthYears * 31557600)
   where yearToEarthYears = case planet of
           "Mercury" -> 0.2408467
-          "Venus" -> 0.61519726
-          "Earth" -> 1.0
-          "Mars" -> 1.8808158
+          "Venus"   -> 0.61519726
+          "Earth"   -> 1.0
+          "Mars"    -> 1.8808158
           "Jupiter" -> 11.862615
-          "Saturn" -> 29.447498
-          "Uranus" -> 84.016846
+          "Saturn"  -> 29.447498
+          "Uranus"  -> 84.016846
           "Neptune" -> 164.79132
-          "Pluto" -> error "Pluto is not a planet"
-          _ -> error ("Unrecognized planet name: '" ++ planet ++ "'")
+          "Pluto"   -> error "Pluto is not a planet"
+          _         -> error ("Unrecognized planet name: '" ++ planet ++ "'")
 
 isLeapYear :: Int -> Bool
 isLeapYear year
