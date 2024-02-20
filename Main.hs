4c28c491c142970012bec80330ef37e8b7ad0901
dimaStepul <trushaka2001@gmail.com>
2024-02-20 14:26:50 +0200

    hw1 done

--- Main.hs
+++ Main.hs
@@ -1,16 +1,30 @@
 module Main where
-
 import Control.Monad (unless)
 import Text.Printf (printf)
 
 ageOn :: String -> Float -> Float
-ageOn planet ageInSeconds =
-  undefined
+ageOn planet ageInSeconds = ageInSeconds / (orbitalPeriod planet * earthYearInSeconds)
+  where
+    earthYearInSeconds = 31557600
+    orbitalPeriod planet = case planet of
+      "Mercury" -> 0.2408467
+      "Venus"   -> 0.61519726
+      "Earth"   -> 1.0
+      "Mars"    -> 1.8808158
+      "Jupiter" -> 11.862615
+      "Saturn"  -> 29.447498
+      "Uranus"  -> 84.016846
+      "Neptune" -> 164.79132
+      "Pluto"   -> error "Pluto is not a planet"
+      _         -> error "Unknown planet"
 
 isLeapYear :: Int -> Bool
-isLeapYear year =
-  undefined
-
+isLeapYear year
+  | year < 0 = error " cannot be negative"
+  | year `mod` 400 == 0 = True
+  | year `mod` 100 == 0 = False
+  | year `mod` 4 == 0 = True
+  | otherwise = False
 main = do 
   runTests
   putStrLn "Done"
