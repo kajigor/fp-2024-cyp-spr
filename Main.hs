b116c4577f1fa23fc011bfd496e43385f5276ac1
George Gerasev <george30032002@gmail.com>
2024-02-24 19:23:33 +0200

    solution

--- Main.hs
+++ Main.hs
@@ -3,13 +3,31 @@ module Main where
 import Control.Monad (unless)
 import Text.Printf (printf)
 
+orbitalPerioudOf :: String -> Float
+orbitalPerioudOf planetName = 
+  case planetName of
+    "Mercury" -> 0.2408467
+    "Venus" -> 0.61519726
+    "Earth" -> 1.0
+    "Mars" -> 1.8808158
+    "Jupiter" -> 11.862615
+    "Saturn" -> 29.447498
+    "Uranus" -> 84.016846
+    "Neptune" -> 164.79132
+
+
 ageOn :: String -> Float -> Float
 ageOn planet ageInSeconds =
-  undefined
+  (ageInSeconds / secondsInEarthYear) / orbitalPerioudOf planet
+  where
+    secondsInEarthYear = 31557600
 
 isLeapYear :: Int -> Bool
-isLeapYear year =
-  undefined
+isLeapYear year
+  | mod year 400 == 0 = True
+  | mod year 100 == 0 = False
+  | mod year 4 == 0 = True
+  | otherwise = False
 
 main = do 
   runTests
