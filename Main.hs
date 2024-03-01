ce711a10ade04a8df31ea4448fa6e80a17b39fae
imanninen <igor.manninen@gmail.com>
2024-03-01 10:42:41 +0200

    Fix some problems

--- Main.hs
+++ Main.hs
@@ -19,15 +19,11 @@ instance Show Operation where
 
 instance Eq Operation where 
   Plus == Plus = True
-  Plus == _ = False
   Minus == Minus = True
-  Minus == _ = False
   Mult == Mult = True
-  Mult == _ = False
   Div == Div = True
-  Div == _ = False
   Pow == Pow = True
-  Pow == _ = False
+  _ == _ = False
 
 data Expr = 
   Const Double
@@ -37,12 +33,13 @@ data Expr =
 instance Show Expr where 
   show (Const a) = show a
   show (Sqrt exp) = printf " sqrt( %s )" (show exp)
-  show (Bin exp1 oper exp2) = show exp1 ++ show oper ++ show exp2
+  show (Bin exp1 oper exp2) = printf "(%s)" (show exp1 ++ show oper ++ show exp2)
 
 instance Eq Expr where 
   Const a == Const b = a == b
   Sqrt exp1 == Sqrt exp2 = exp1 == exp2
   Bin a1 op1 b1 == Bin a2 op2 b2 = op1 == op2 && a1 == a2 && b1 == b2
+  a == b = eval a == eval b
   -- (==) = undefined 
 
 data Error = DivByZero [Char]
@@ -54,9 +51,9 @@ instance Show Error where
   show (NegativeRoot msg) = printf "ERROR_NEGATIVE_ROOT: %s" msg
 
 instance Eq Error where 
-  DivByZero msg1 == DivByZero msg2 = msg1 == msg2
-  NegativeRoot msg1 == NegativeRoot msg2 = msg1 == msg2
-  -- (==) = undefined 
+  DivByZero _ == DivByZero _ = True
+  NegativeRoot _ == NegativeRoot _ = True
+  _ == _ = True
 
 eval :: Expr -> Either Error Double 
 eval (Const a) = Right a
@@ -72,21 +69,27 @@ eval (Bin exp1 op exp2)
   | op == Plus = resolveOperation exp1 (+) exp2
   | op == Minus = resolveOperation exp1 (-) exp2
   | op == Mult = resolveOperation exp1 (*) exp2
-  | op == Div = let ans1 = eval exp1 in
-    let ans2 = eval exp2 in
-    if isLeft ans1 then ans1 else 
-      if isLeft ans2 then ans2 else
-        if fromRight 1 ans2 == 0 then Left $ DivByZero $ show $ fromRight 1 ans1
-        else resolveOperation exp1 (/) exp2
+  | op == Div = 
+    let ans1 = eval exp1
+        ans2 = eval exp2 in
+    case (ans1, ans2) of 
+      (Right _, Left _) -> ans2
+      (Left msg, _) -> ans1
+      (Right d1, Right 0.0) -> Left $ DivByZero $ show d1
+      _ -> resolveOperation exp1 (/) exp2
   | op == Pow = resolveOperation exp1 (**) exp2
 
 resolveOperation :: Expr -> (Double -> Double -> Double) -> Expr -> Either Error Double
 resolveOperation exp1 op exp2 =
-  let ans1 = eval exp1 in
-    let ans2 = eval exp2 in
-    if isLeft ans1 then ans1 else 
-      if isLeft ans2 then ans2 else
-        Right $ fromRight 1 ans1 `op` fromRight 1 ans2
+  let ans1 = eval exp1 
+      ans2 = eval exp2 in
+  case (ans1, ans2) of
+    (Right _, Left _) -> ans2
+    (Left msg, _) -> ans1
+    (Right d1, Right d2) -> Right $ d1 `op` d2
+    -- if isLeft ans1 then ans1 else 
+    --   if isLeft ans2 then ans2 else
+    --     Right $ fromRight 1 ans1 `op` fromRight 1 ans2
 
 
 cases :: [(Expr, Either Error Double)]
