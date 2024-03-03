781bf1ca06dc64cfce49cd56eea64121629d1cc0
Georgii Ustinov <ustinovgo@gmail.com>
2024-03-03 09:19:49 +0200

    First 5.5 tasks done

--- Main.hs
+++ Main.hs
@@ -5,10 +5,26 @@ import Control.Monad (unless)
 import Data.Either (fromRight, fromLeft)
 import Language.Haskell.TH (Exp)
 
-data Expr = Const Double | Add Expr Expr | Subtract Expr Expr | Multiply Expr Expr | Divide Expr Expr | Power Expr Expr | Square Expr
+newtype MyEither a b = MyEither (Either a b) deriving (Show)
 
-instance Show Expr where
-  show :: Expr -> String
+instance Functor (MyEither err) where
+    fmap :: (f -> t) -> MyEither err f -> MyEither err t
+    fmap _ (MyEither (Left x)) = MyEither(Left x)
+    fmap f (MyEither (Right x)) = MyEither(Right (f x))
+
+
+newtype MyArrow a b = MyArrow((->) a b)
+
+instance Functor (MyArrow f) where
+    fmap :: (a -> b) -> MyArrow f a -> MyArrow f b
+    fmap f (MyArrow g)  = MyArrow (f . g)
+
+
+
+data Expr a = Const a | Var String | Add (Expr a) (Expr a) | Subtract (Expr a) (Expr a) | Multiply (Expr a) (Expr a) | Divide (Expr a) (Expr a) | Power (Expr a) (Expr a) | Square (Expr a) deriving (Eq)
+
+instance (Show a) => Show (Expr a) where
+  show :: Expr a -> String
   show (Const x) = show x
   show (Add x y) = "(" ++ show x ++ " + " ++ show y ++ ")"
   show (Subtract x y) = "(" ++ show x ++ " - " ++ show y ++ ")"
@@ -16,25 +32,19 @@ instance Show Expr where
   show (Divide x y) = "(" ++ show x ++ " / " ++ show y ++ ")"
   show (Power x y) = show x ++ " ^ " ++ show y
   show (Square x) = "sqrt(" ++ show x ++ ")"
+  show (Var name) = name
 
 
-instance Eq Expr where
-  (==) :: Expr -> Expr -> Bool
-  (==) a b =
-     let fst = eval a
-         snd = eval b
-     in fst == snd 
-
-
-data Error = DivisorIsZero | SquareRootIsNegative | PowerBaseIsNegative deriving Eq
+data Error = DivisorIsZero | SquareRootIsNegative | PowerBaseIsNegative | VariableIsUndefined deriving Eq
 instance Show Error where
   show :: Error -> String
   show DivisorIsZero = "Divisor is equal zero"
   show SquareRootIsNegative  = "The root can't be negative"
   show PowerBaseIsNegative  = "Power base can't be negative"
+  show VariableIsUndefined = "Variable is undefined"
 
 
-performOp :: (Double -> Double -> Double) -> Either Error Double -> Either Error Double -> Either Error Double
+performOp :: (a -> a -> a) -> Either Error a -> Either Error a -> Either Error a
 performOp f a b = case a of
     Right fst -> case b of
       Right snd -> Right (f fst snd)
@@ -42,19 +52,19 @@ performOp f a b = case a of
     err -> err
 
 
-performOpWithCheck :: (Either Error Double -> Either Error Double -> Maybe Error) -> (Double -> Double -> Double) -> Either Error Double -> Either Error Double -> Either Error Double
+performOpWithCheck :: (Either Error a -> Either Error a -> Maybe Error) -> (a -> a -> a) -> Either Error a -> Either Error a -> Either Error a
 performOpWithCheck pr f a b = case pr a b of
   Nothing -> performOp f a b
   Just err -> Left err
 
-checkDivisorIsZero :: Either Error Double -> Either Error Double -> Maybe Error
+checkDivisorIsZero :: (Eq a, Fractional a) => Either Error a -> Either Error a -> Maybe Error
 checkDivisorIsZero _ b = case b of 
   Right 0.0 -> Just DivisorIsZero
   Right _ -> Nothing
   Left err -> Just err
 
 
-checkFirstArgIsNegative :: Error -> Either Error Double -> Either Error Double -> Maybe Error
+checkFirstArgIsNegative :: (Ord a, Num a) => Error -> Either Error a -> Either Error a -> Maybe Error
 checkFirstArgIsNegative err (Right x) _
   | x < 0 = Just err
   | otherwise = Nothing
@@ -62,19 +72,90 @@ checkFirstArgIsNegative err (Right x) _
 checkFirstArgIsNegative err a _ = Just (fromLeft err a)
 
 
-eval :: Expr -> Either Error Double
-eval expr = case expr of
-  Const val -> Right val
-  Add a b -> performOp (+) (eval a) (eval b)
-  Subtract a b -> performOp (-) (eval a) (eval b)
-  Multiply a b -> performOp (*) (eval a) (eval b)
-  Divide a b -> performOpWithCheck checkDivisorIsZero (/) (eval a) (eval b)
-  Power a b -> performOpWithCheck (checkFirstArgIsNegative PowerBaseIsNegative) (**) (eval a) (eval b)
-  Square a -> performOpWithCheck (checkFirstArgIsNegative SquareRootIsNegative) (**) (eval a) (Right 0.5)
+getVar :: [(String, a)] -> String -> Either Error a
+getVar map name = case filter (\x -> fst x == name) map of
+  xs:x -> Right (snd xs)
+  _ -> Left VariableIsUndefined
 
-cases :: [(Expr, Either Error Double)]
+
+eval :: (Ord a, Floating a) => Expr a -> [(String, a)] -> Either Error a
+eval expr map = case expr of
+  Const val -> Right val
+  Var name -> getVar map name
+  Add a b -> performOp (+) (eval a map) (eval b map)
+  Subtract a b -> performOp (-) (eval a map) (eval b map)
+  Multiply a b -> performOp (*) (eval a map) (eval b map)
+  Divide a b -> performOpWithCheck checkDivisorIsZero (/) (eval a map) (eval b map)
+  Power a b -> performOpWithCheck (checkFirstArgIsNegative PowerBaseIsNegative) (**) (eval a map) (eval b map)
+  Square a -> performOpWithCheck (checkFirstArgIsNegative SquareRootIsNegative) (**) (eval a map) (Right 0.5)
+
+
+simplifyAdd :: (Eq a, Fractional a) => Expr a -> Expr a
+simplifyAdd expr = case expr of
+  Add (Const 0.0) x -> x
+  Add x (Const 0.0) -> x
+  _ -> expr
+
+simplifySubtract :: (Eq a, Fractional a) => Expr a -> Expr a
+simplifySubtract expr = case expr of
+  Subtract x (Const 0.0) -> x
+  _ -> expr
+
+
+chooseMultiplyByZeroSimplification :: Fractional a => Expr a -> Expr a
+chooseMultiplyByZeroSimplification expr = case expr of
+  Const _ -> Const 0.0
+  Var _ -> Const 0.0
+  _ -> expr
+
+simplifyMultiply :: (Eq a, Fractional a) => Expr a -> Expr a
+simplifyMultiply expr = case expr of
+  Multiply (Const 0.0) x -> chooseMultiplyByZeroSimplification x
+  Multiply x (Const 0.0) -> chooseMultiplyByZeroSimplification x
+  Multiply (Const 1.0) x -> x
+  Multiply x (Const 1.0) -> x
+  _ -> expr
+
+simplifyDivide :: (Eq a, Fractional a) => Expr a -> Expr a
+simplifyDivide expr = case expr of
+  Divide (Const 0.0) (Const x)
+    | x /= 0 -> Const 0.0
+    | otherwise -> expr 
+  Divide x (Const 1.0) -> x
+  Divide (Const x) (Const y)
+    | x == y && x /= 0.0 -> Const 1.0
+    | otherwise -> expr
+  _ -> expr
+
+
+
+simplifyPower :: (Fractional a, Ord a) => Expr a -> Expr a
+simplifyPower expr = case expr of
+  Power (Const x) (Const 1.0)
+     | x >= 0.0 -> Const x
+     | otherwise -> expr
+  Power (Const x) (Const 0.0)
+     | x >= 0.0 -> Const 1.0
+     | otherwise -> expr
+  _ -> expr
+
+simplify :: (Ord a, Fractional a) =>  Expr a -> Expr a
+simplify expr = case expr of
+  Add l r -> simplifyAdd (Add (simplify l) (simplify r))
+  Subtract l r -> simplifySubtract (Subtract (simplify l) (simplify r))
+  Multiply l r -> simplifyMultiply (Multiply (simplify l) (simplify r))
+  Divide l r -> simplifyDivide (Divide (simplify l) (simplify r))
+  Power l r -> simplifyPower (Power (simplify l) (simplify r))
+  Square l -> Square (simplify l)
+  _ -> expr
+
+
+
+cases :: Fractional a => [(Expr a, Either Error a)]
 cases = [
   (Const 1, Right 1.0),
+  (Var "x", Right 1.0),
+  (Var "undefined", Left VariableIsUndefined),
   (Add (Const 1) (Const 1), Right 2.0),
   (Subtract (Const 1) (Const 1), Right 0.0),
   (Multiply (Const 2) (Const 3), Right 6.0),
@@ -86,12 +167,14 @@ cases = [
   (Power (Const (-3)) (Const 3), Left PowerBaseIsNegative),
   (Add (Multiply (Const 5) (Const 3)) (Divide (Subtract (Const 15) (Const 5)) (Const 5)), Right 17.0),
   (Square (Square (Square (Const 256.0))), Right 2.0),
-  (Power (Const 2) (Power (Const 2) (Const 2)), Right 16)
+  (Power (Const 2) (Power (Const 2) (Const 2)), Right 16),
+  (Power (Var "y") (Var "z"), Right 8)
   ]
 
-test :: Expr -> Either Error Double -> IO ()
+test :: (Show a, Ord a, Floating a) => Expr a -> Either Error a -> IO ()
 test expr expected =
-    let actual = eval expr in
+    let testMap = [("x", 1), ("y", 2), ("z", 3)]
+        actual = eval expr testMap in
     unless (expected == actual) $ describeFailure actual
   where
     describeFailure actual =
