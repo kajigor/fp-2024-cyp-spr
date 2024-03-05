76ac94772ccced00ca58f1deab950023cc373ff6
F47-503 <vitaly14kurin@gmail.com>
2024-03-05 21:57:47 +0200

    HW4: same pattern moved to another function

--- Main.hs
+++ Main.hs
@@ -99,30 +99,31 @@ instance Floating a => Num (Expr a) where
   fromInteger val = Expr (fromInteger val)
   negate a = a * (-1)
 
+manageArithmetics :: (Floating a, Eq a, Show a) => (Expr a -> Expr a -> Expr a) -> [(Maybe (Expr a), Maybe (Expr a), Maybe (Expr a))] -> Expr a -> Expr a -> Expr a
+manageArithmetics op patterns x y =
+  let simpleX = simplify x
+      simpleY = simplify y
+  in let  matchPattern (Just p1, _, Just val) = if simpleX == p1 then Just val else Nothing
+          matchPattern (_, Just p2, Just val) = if simpleY == p2 then Just val else Nothing
+          matchPattern (Just p1, _, _) = if p1 == simpleX then Just simpleY else Nothing
+          matchPattern (_, Just p2, _) = if p2 == simpleY then Just simpleX else Nothing
+          matchPattern _ = Nothing
+  in let matchedPatterns = filter (Nothing /=) (map matchPattern patterns)
+  in case matchedPatterns of
+    [] -> op simpleX simpleY
+    (Just res):t -> res
+
 simplify :: (Floating a, Eq a, Show a) => Expr a -> Expr a
 simplify (Expr a) = Expr a
 simplify (Var x) = Var x
 simplify ((:-) x y) = if x == y then Expr 0.0 else simplify x :- simplify y
-simplify (x :+ y) = 
-  case (simplify x, simplify y) of
-    (Expr 0.0, newY) -> newY 
-    (newX, Expr 0.0) -> newX 
-    _ -> simplify x :+ simplify y
-simplify (x :* y) = 
-  case (simplify x, simplify y) of
-    (Expr 0.0, newY) -> Expr 0.0 
-    (newX, Expr 0.0) -> Expr 0.0
-    (Expr 1.0, newY) -> newY 
-    (newX, Expr 1.0) -> newX 
-    _ -> simplify x :* simplify y
-simplify (x :/ y) = 
-  case (simplify x, simplify y) of
-    (newX, Expr 1.0) -> newX
-    _ -> simplify x :/ simplify y  
-simplify (x :^ y) = 
-  case (simplify x, simplify y) of
-    (newX, Expr 1.0) -> newX
-    _ -> simplify x :^ simplify y    
+simplify (x :+ y) = manageArithmetics (:+) [(Just (Expr 0.0), Nothing, Nothing), (Nothing, Just (Expr 0.0), Nothing)] x y
+simplify (x :* y) = manageArithmetics (:*) [(Just (Expr 0.0), Nothing, Just (Expr 0.0))
+                                          , (Nothing, Just (Expr 0.0), Just (Expr 0.0))
+                                          , (Just (Expr 1.0), Nothing, Nothing)
+                                          , (Nothing, Just (Expr 1.0), Nothing)] x y
+simplify (x :/ y) = manageArithmetics (:/) [(Nothing, Just (Expr 1.0), Nothing)] x y
+simplify (x :^ y) = manageArithmetics (:^) [(Nothing, Just (Expr 1.0), Nothing)] x y
 
 simplifyCases :: Floating a => [(Expr a, Expr a)]
 simplifyCases = [(Expr 1.0 :+ (Expr 0.0 :* (Expr 2.0 :+ Expr 3.0)) :+ Expr 0.0 :+ (Expr 2.0 :^ Expr 1.0), Expr 1.0 + Expr 2.0) --basic simplify
