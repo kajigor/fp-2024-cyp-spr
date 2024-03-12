d41ae0ff0961004c818dd7f53dda8b5f1458a775
EKCKABATOP54 <androposhtar1029@gmail.com>
2024-03-12 22:27:56 +0100

    cant install tasty)

--- expr/app/Main.hs
+++ expr/app/Main.hs
@@ -1,20 +1,6 @@
-module Main ( main ) where
+module Main (main) where
 
-import Expr ( Expr (..) )
-import Simplify ( simplify )
-import Text.Printf ( printf )
-
-printSimplified :: Expr Double -> IO () 
-printSimplified expr = do 
-  putStrLn $ printf "Expr:\t\t%s\nSimplified:\t%s\n\n" (show expr) (show $ simplify expr)
+import Lib
 
 main :: IO ()
-main = do 
-  printSimplified $ 0
-  printSimplified $ 0 * 1 
-  printSimplified $ 1 * 0 
-  printSimplified $ 0 - 1
-  printSimplified $ 1 - 0
-  printSimplified $ 1 - 1  
-  printSimplified $ 0 * 1 + 2 * 3 - (Var "v") * 0 - 2 * 3 
-  
+main = someFunc
--- expr/test/Spec.hs
+++ expr/test/Spec.hs
@@ -1,32 +1,70 @@
-import Test.Tasty ( defaultMain, testGroup, TestTree )
-import Test.Tasty.HUnit ( (@?=), testCase, assertBool, assertFailure )
-import Expr ( Expr (..), BinOp (..) )
-import Eval ( eval )
-import qualified Data.Map.Strict as M 
 
-testEval :: TestTree
-testEval = 
-    testGroup "Eval" [ testAdd, testPow ]
+import Expr
+import Control.Monad (unless)
+import Text.Printf (printf)
+
+{-
+type ExprConstCaseType = Double
+
+cases :: [(Expr ExprConstCaseType, [(String, ExprConstCaseType)] , Either (Error ExprConstCaseType) ExprConstCaseType)]
+cases = [ 
+        (Const 777, [],  Right 777),
+        (Pow (Div (Mult (Minus (Pow (Plus (SquareRoot(Const 25)) (Const 3)) (Const 2)) (Mult (Const 4) (Const 7))) (Const 2)) (Const 5)) (Const 3), [],  Right ( ((  ( ( sqrt 25 + 3)**2 - 4 * 7) * 2) / 5)**3)),  
+        let e1 = Mult (Const 4) (Const 5)
+            e2 = Plus (Const 1) (Const (-1)) in
+            (Div e1 e2, [],  Left (DivisionByZero e1 e2)),
+        let e = Minus (Const 1) (Const 100) in
+        (SquareRoot e, [],  Left (RootOfNegative e)),
+        (Var "x", [("x", 100)], Right 100),
+        (Plus (Var "x") (Var "y"), [("x", 10.0), ("y", 20.0)], Right 30.0),
+        (Plus (Var "z") (Var "y"), [("x", 10), ("y", 20)], Left $ UndefinedVariable "z"),
+        (Minus (Var "x") (Var "x"), [("x", 777)], Right 0), 
+        (Minus (Var "x") (Var "x"), [("x", 14)], Right 0), 
+        (Pow (Var "x") (Var "y"), [("x", 2), ("y", 10)], Right 1024 ),
+        (Pow (Var "x") (Var "y"), [("x", 2), ("y", 0)], Right 1 ),
+        (Pow (Var "x") (Var "y"), [("x", 0), ("y", 0)], Right 1 ),
+        (Plus (Var "x") (Var "y"), [("x", 10), ("x", 20), ("y", 20)], Left $ InvalidMapping [("x", 10), ("x", 20)]),
+        (Plus (Var "x") (Var "y"), [("x", 10), ("x", 20), ("y", 20), ("y", 30)], Left $ InvalidMapping [("x", 10), ("x", 20), ("y", 20), ("y", 30)]),
+        (Plus (Var "x") (Var "y"), [("x", 10), ("x", 20), ("y", 30)], Left $ InvalidMapping [("x", 10), ("x", 20)])
+        ] 
+
+test :: Expr ExprConstCaseType -> [(String, ExprConstCaseType)]  -> Either (Error ExprConstCaseType) ExprConstCaseType -> IO () 
+test expr var_list expected = 
+    let actual = eval expr var_list in 
+    unless (expected == actual) $ describeFailure actual
   where 
-    testEvalNoVarSuccess msg expr res = 
-       testCase msg $ eval expr M.empty @?= Right res 
-    testAdd = testGroup "Add" 
-      [ testCase "1 + 2 == 3" $ eval (BinOp Plus (Num 1) (Num 2)) M.empty @?= Right 3
-      , testCase "2 + 1 == 3" $ eval (BinOp Plus (Num 2) (Num 1)) M.empty @?= Right 3
-      , testCase "2 + 1 == 3 as Num instance" $ eval (2+1) M.empty @?= Right 3
-      , testEvalNoVarSuccess "0+2 == 2" (0+2) 2
-      ]
-    testPow = testGroup "Pow" 
-      [ -- testEvalNoVarSuccess "(-4) ** 0.5 == NaN" (BinOp Pow (Num (-4)) (Num (0.5))) ((-4) ** 0.5) -- always fail due to NaN /= NaN
-        testNaN "(-4) ** 0.5 == NaN" (BinOp Pow (Num (-4)) (Num (0.5)))
-      ]
-    testNaN msg expr = testCase msg $ 
-      case eval expr M.empty of 
-        Right x -> assertBool "Should be NaN" (isNaN x)
-        Left x -> assertFailure $ "Evaluation produced an error " ++ show x 
+    describeFailure actual = 
+      printf "eval (%s) should be %s but it was %s\n" (show expr) (show expected) (show actual) 
     
+simplifyTests :: [(Expr ExprConstCaseType, Expr ExprConstCaseType)]
+simplifyTests = [ (Mult (Const 0) (Const 5), Const 0),
+          (Plus (Mult (Const 1) (Var "x")) (Const 0), Var "x"),
+          (Plus (Const 0) (       
+                Mult  (Const 1) (    
+                      Minus (Div (Const 42) (Const 1) ) (Const 0) 
+                )
+                )
+        , Const 42), 
+        (Plus (Const 1) (Mult (Const 0) (Var "x") ), Const 1), 
+        (Minus (Const 1) (Const 1), Const 0),
+        (Minus (Var "x") (Var "x"), Const 0),
+        let e = Pow (Div (Mult (Minus (Pow (Plus (SquareRoot(Const 25)) (Const 3)) (Const 2)) (Mult (Const 4) (Const 7))) (Const 2)) (Const 5)) (Const 3) in
+        (Minus e e, Const 0),
+        (Pow (Div (Mult (Minus (Pow (Plus (SquareRoot(Const 25)) (Const 3)) (Const 2)) (Mult (Const 4) (Const 7))) (Const 2)) (Const 5)) (Const 3), Pow (Div (Mult (Minus (Pow (Plus (SquareRoot(Const 25)) (Const 3)) (Const 2)) (Mult (Const 4) (Const 7))) (Const 2)) (Const 5)) (Const 3)),
+        (Pow (Div (Mult (Minus (Pow (Plus (SquareRoot(Const 25)) (Var "x")) (Const 2)) (Mult (Const 4) (Const 7))) (Const 2)) (Const 5)) (Const 3), Pow (Div (Mult (Minus (Pow (Plus (SquareRoot(Const 25)) (Var "x")) (Const 2)) (Mult (Const 4) (Const 7))) (Const 2)) (Const 5)) (Const 3))]
 
 
+testSimplify :: Expr ExprConstCaseType -> Expr ExprConstCaseType-> IO () 
+testSimplify exprToSimplify expectedExpr = 
+    let actual = simplify exprToSimplify in 
+    unless (expectedExpr == actual) $ describeFailure actual
+  where 
+    describeFailure actual = 
+      printf "simplify (%s) should be %s but it was %s\n" (show exprToSimplify) (show expectedExpr) (show actual) 
+
+
+-}
 main :: IO ()
-main = 
-  defaultMain $ testGroup "Expressions" [ testEval ]
+main =
+    putStrLn "d"
+--  defaultMain $ testGroup "Empty" []
