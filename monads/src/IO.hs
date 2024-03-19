c80b8b6a78631c172dbff90cf88405f96e41ffa5
George Gerasev <george30032002@gmail.com>
2024-03-19 16:35:54 +0200

    IO

--- monads/src/IO.hs
+++ monads/src/IO.hs
@@ -3,14 +3,28 @@ module IO where
 -- Implement reading line from standard input. 
 -- Use getChar to read a single character. 
 myGetLine :: IO String 
-myGetLine = undefined 
+myGetLine = getLineHelper
+    where getLineHelper :: IO String
+          getLineHelper = do
+                c <- getChar
+                if c == '\n'
+                    then return []
+                    else (c:) <$> getLineHelper -- what have I created
+                        
+
 
 -- Ask the user for their name.
 -- Print "Hello, NAME" to the standard output, where NAME is the name of the user.
 -- Use myGetLine.
 helloUser :: IO () 
-helloUser = undefined 
+helloUser = do
+    putStrLn "What is your name?"
+    name <- myGetLine
+    putStrLn ("Hello, " ++ name ++ ".")
+
 
 -- Use interact in helloUser.
 helloUser' :: IO () 
-helloUser' = undefined 
+helloUser' = do 
+    putStrLn "What is your name?"
+    interact (\name -> "Hello" ++ name ++ ".")
