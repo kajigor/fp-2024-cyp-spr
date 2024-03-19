29aeb2217b4a58f410787870c92f7143bd62d5e2
imanninen <igor.manninen@gmail.com>
2024-03-19 14:12:16 +0200

    Either and List done

--- monads/src/Either.hs
+++ monads/src/Either.hs
@@ -6,10 +6,51 @@ data MyEither a b
   deriving (Show, Eq)
 
 -- Implement the instance and prove the laws
-instance Functor (MyEither a)
+instance Functor (MyEither a) where 
+  fmap :: (b -> c) -> MyEither a b -> MyEither a c
+  fmap _ (MyLeft a) = MyLeft a
+  fmap f (MyRight b) = MyRight (f b)
+-- Proof why it works:
+-- id: 
+--     fmap id (MyLeft a) = MyLeft a
+--     fmap id (MyRight b) = MyRight (id b) = MyRight b
+-- Composition:
+--     fmap (f . g) (MyLeft a) = MyLeft a
+--     fmap (f . g) (MyRight b) = MyRight (f . g) b
+--
+-- Implement the instance and prove the laws
+instance Applicative (MyEither a) where
+  pure :: b -> MyEither a b
+  pure = MyRight
+  (<*>) :: MyEither a (b -> c) -> MyEither a b -> MyEither a c
+  (<*>) (MyRight g) (MyRight a) = MyRight $ g a  
+  (<*>) (MyLeft a) _ = MyLeft a
+  (<*>) _ (MyLeft a) = MyLeft a
+-- Prove of app law:
+-- I'm looking on not trivial cases!
+-- 1) (pure id) <*> (MyRight b) === (MyRight id) <*> (MyRight b) === MyRight b - true
+-- (pure id) <*> (MyLeft a) === (Right id) <*> (MyLeft a) === MyLeft a - true
+--
+-- 2) (pure) (.) <*> u <*> v <*> w === MyRight (.) <*> MyRight a <*> MyRight b <*> w ===
+-- MyRight (. a) <*> v <*> w === MyRight (a . b) <*> w === MyRight ((a . b) w) =?=
+-- u <*> (v <*> w) === u <*> (MyRight (b w)) === MyRight ((a . b) w)  - true
+--
+-- 3) (pure f) <*> (pure b) === (MyRight f) <*> (MyRight b) === MyRight (f b)
+--
+-- 4) u <*> pure y === MyRight a <*> pure y === MyRight (a y) =?= 
+-- pure ($ y) <*> u === MyRight ($ y) <*> MyRight a === MyRight ($ y a) === MyRight (a y) - true
 
 -- Implement the instance and prove the laws
-instance Applicative (MyEither a) 
+instance Monad (MyEither a) where 
+  return :: b -> MyEither a b
+  return = pure
 
--- Implement the instance and prove the laws
-instance Monad (MyEither a)
+  (>>=) :: MyEither a b -> (b -> MyEither a c) -> MyEither a c
+  (>>=) (MyRight b) f =  f b
+  (>>=) (MyLeft a) _ = MyLeft a
+-- Prove of monade law:
+-- 1) return b >>= k === MyRight b >>= k === k b 
+-- 2) m >>= return === Eather a b >>= return === Eather a b === m
+-- 3) m >>= (\x -> k x >>= h) === Eather a b >>= (\x -> k x >>= h) ===
+-- k (a|b) >>= h === h (a|b) =?= m >>= k >>= h === Eather a b >>= k >>= h ===
+-- k (a|b) >>= h === h (a|b) - true
--- monads/src/List.hs
+++ monads/src/List.hs
@@ -1,15 +1,71 @@
 module List where 
+import Language.Haskell.TH (Con)
 
 data List a 
-  = Nil 
-  | Cons a (List a)
+  = Nil |
+  Cons a (List a)
   deriving (Show, Eq)
 
 -- Implement the instance and prove the laws
-instance Functor List 
+instance Functor List where
+  fmap :: (a -> b) -> List a -> List b
+  fmap f Nil = Nil
+  fmap f (Cons x xs) = Cons (f x) (fmap f xs)
+-- Proof why it works:
+-- id: 
+--     fmap id Nil = Nil
+--     fmap id (Cons x xs) = Cons (id x) (fmap id xs) = Cons x xs
+-- Composition:
+--     fmap (f . g) Nil = Nil
+--     fmap (f . g) (Cons x xs) = Cons ((f . g) x) (fmap (f . g) xs) = Cons ((f . g) x) ((f.g) xs)
+-- here i mean than function (f . g) will be applyed to each element of xs.
+
+concatList :: List a -> List a -> List a
+concatList Nil xs = xs
+concatList xs Nil = xs
+concatList (Cons x Nil) ys = Cons x ys
+concatList (Cons x xs) ys = Cons x (concatList xs ys)
 
 -- Implement the instance and prove the laws
-instance Applicative List
-
+instance Applicative List where 
+  pure :: a -> List a
+  pure x = Cons x Nil
+  (<*>) :: List (b -> c) -> List b -> List c
+  (<*>) (Cons f fs) xs = concatList (fmap f xs) (fs <*> xs)
+  (<*>) Nil _ = Nil
+-- Prove of app law:
+--
+-- 1) (pure id) <*> (Cons x xs) === (Cons id Nil) <*> (Cons x xs) === Cons x xs
+-- (pure id) <*> Nil === (Cons id Nil) <*> Nil === Nil
+-- 
+-- 2) pure (.) <*> u <*> v <*> w === (Cons (.) Nil) <*> (Cons x xs) <*> (Cons y ys) <*> w ===
+-- Cons (. x) (. xs) <*> (Cons y ys) <*> w === Cons (x . y) (xs .each ys) <*> w ===
+-- Cons (x . y) (xs .each ys) <$> w =?=  Cons x xs <*> (Cons y ys <*> w) === 
+-- Cons x xs <*> (Cons y ys <$> w) === Cons (x . y) (xs .each ys) <$> w - true
+-- 
+-- 3) (pure f) <*> (pure b) === (Cons f fs) <*> (Cons y ys) === Cons (f x) (fs "each" ys) 
+--
+-- 4) u <*> pure y === Cons x xs <*> Cons y Nil === Cons (x y) xs =?=
+-- pure ($ y) <*> u === Cons ($ y) <*> Cons x xs === Cons ($ y x) <*> xs === Cons (x y) xs
+-- 
 -- Implement the instance and prove the laws
-instance Monad List
+instance Monad List where
+  return :: a -> List a
+  return = pure
+
+  (>>=) :: List a -> (a -> List b) -> List b
+  (>>=) (Cons x xs) f = concatList (f x) (xs >>= f) 
+  (>>=) Nil _ = Nil
+-- Prove of monade law:
+-- 
+-- 1) return b >>= k === Cons b Nil >>= k === concatList (k b) (Nil >>= k) === k b - true
+--
+-- 2) m >>= return === Cons x xs >>= return === ConcatList (return x) (xs >>= return) ===
+-- ConcatList (Cons x Nil) (xs >>= return) === m
+--
+-- 3)  m >>= (\x -> k x >>= h) === Cons x xs >>= (\x -> k x >>= h) === 
+-- concatList (k x >>= h) (xs >>= (\x -> k x >>= h)) =?= 
+-- m >>= k >>= h === Cons x xs >>= k >>= h === concatList (k x) (xs >>= k) >>= h ===
+-- Cons (k x) (k each xs) >>= h === concatList (h (k x)) (k each xs >>= h)
+-- if think carefully, we can see that this two lists are equale.
+--
