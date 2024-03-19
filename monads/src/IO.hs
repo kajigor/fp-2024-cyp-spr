ccc1f82bff613b792c3d9f24a55e932fd1485aa3
imanninen <igor.manninen@gmail.com>
2024-03-19 20:51:51 +0200

    IO

--- monads/src/IO.hs
+++ monads/src/IO.hs
@@ -1,16 +1,24 @@
 module IO where 
+import Control.Concurrent (getChanContents)
 
 -- Implement reading line from standard input. 
 -- Use getChar to read a single character. 
 myGetLine :: IO String 
-myGetLine = undefined 
+myGetLine = do 
+  ch <- getChar 
+  if ch == '\n' then return "" else myGetLine >>= (\x -> return (ch:x)) 
 
 -- Ask the user for their name.
 -- Print "Hello, NAME" to the standard output, where NAME is the name of the user.
 -- Use myGetLine.
 helloUser :: IO () 
-helloUser = undefined 
+helloUser = do
+  putStrLn "Enter your name: "
+  name <- myGetLine 
+  putStrLn ("Hello, " ++ name)
 
 -- Use interact in helloUser.
 helloUser' :: IO () 
-helloUser' = undefined 
+helloUser' = do
+  putStrLn "Enter your name: "
+  interact ("Hello, " ++)
