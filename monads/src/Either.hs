cdf8e5b9c66cc91369af70917f16857f618ab15f
GrigoriiSolnyshkin <grisolnyshkin04@gmail.com>
2024-03-19 02:19:42 +0200

    List and Either implemented

--- monads/src/Either.hs
+++ monads/src/Either.hs
@@ -1,15 +1,68 @@
-module Either where 
+module Either where
 
-data MyEither a b 
-  = MyLeft a 
-  | MyRight b 
+data MyEither a b
+  = MyLeft a
+  | MyRight b
   deriving (Show, Eq)
 
 -- Implement the instance and prove the laws
-instance Functor (MyEither a)
+instance Functor (MyEither a) where
+  fmap _ (MyLeft left) = MyLeft left
+  fmap func (MyRight right) = MyRight $ func right
+
+{-
+fmap id (MyLeft x) = MyLeft x
+fmap id (MyRight x) = MyRight $ id x = MyRight $ x
+
+fmap (f . g) (MyLeft x) = MyLeft x
+fmap f (fmap g $ MyLeft x) = fmap f $ MyLeft x = MyLeft x
+fmap (f . g) $ MyRight x = MyRight $ (f . g) x
+fmap f (fmap g $ MyRight x) = fmap f (MyRight $ g x) = MyRight $ f (g x)
+-}
+
 
 -- Implement the instance and prove the laws
-instance Applicative (MyEither a) 
+instance Applicative (MyEither a) where
+  pure = MyRight
+  (<*>) (MyRight func) (MyRight right) = MyRight $ func right
+  (<*>) (MyLeft left) _ = MyLeft left
+  (<*>) _ (MyLeft left) = MyLeft left
+
+{-
+pure id <*> MyLeft x = MyLeft x -- as pure generates MyRight
+pure id <*> MyRight x = MyRight id <*> MyRight x = MyRight $ id x = MyRight x
+
+pure f <*> pure x = pure $ f x -- first pattern of <*>
+
+MyLeft left <*> pure x = MyLeft left
+MyRight right <*> pure x = MyRight $ right x
+
+pure ($ x) <*> MyLeft left = MyLeft left 
+pure ($ x) <*> MyRight right = MyRight $ right x
+
+
+pure (.) <*> MyRight x <*> MyRight y <*> MyRight z = MyRight $ ((.) x y) z = MyRight $ x (y z)
+MyRight x <*> (MyRight y <*> MyRight z) = MyRight $ x (y z)
+
+if some of the values is not MyRight then the leftmost MyLeft will be taken in both cases 
+
+-}
 
 -- Implement the instance and prove the laws
-instance Monad (MyEither a)
+instance Monad (MyEither a) where
+  (>>=) (MyLeft left) _ = MyLeft left
+  (>>=) (MyRight right) func = func right
+
+{-
+return a >>= h = h a -- second pattern
+
+(MyLeft left) >>= return = MyLeft left
+(MyRight right) >>= return = return right = MyRight right
+
+((MyLeft left) >>= g) >>= h = MyLeft left = MyLeft left >>= (\x -> g x >>= h)
+
+((MyRight right) >>= g) >>= h = g right >>= h
+MyRight right >>= (\x -> g x >>= h) = g right >>= h
+-}
+
+
--- monads/src/List.hs
+++ monads/src/List.hs
@@ -1,15 +1,80 @@
-module List where 
+module List where
 
-data List a 
-  = Nil 
+data List a
+  = Nil
   | Cons a (List a)
   deriving (Show, Eq)
 
--- Implement the instance and prove the laws
-instance Functor List 
+listFoldr :: (a -> b -> b) -> b -> List a -> b
+listFoldr f init Nil = init
+listFoldr f val (Cons a as) = f a $ listFoldr f val as
+
+listMap :: (a -> b) -> List a -> List b
+listMap f = listFoldr (Cons . f) Nil
+
+listConcat :: List a -> List a -> List a
+listConcat xs ys = listFoldr Cons ys xs
+
+listFlatMap :: (a -> List b) -> List a -> List b
+listFlatMap f = listFoldr (listConcat . f) Nil
+
+
 
 -- Implement the instance and prove the laws
-instance Applicative List
+instance Functor List where
+  fmap f Nil = Nil
+  fmap f (Cons x xs) = Cons (f x) (fmap f xs)
+
+{-
+
+-- all proves will be conducted by "induction"
+fmap id Nil = Nil
+fmap id $ Cons x xs = Cons (id x) (fmap id xs) = Cons x (fmap id xs)
+-- in reality we proved that == will never return false here
+
+fmap (f . g) Nil = Nil
+fmap f (fmap g Nil) = fmap f Nil = Nil
+
+fmap (f . g) $ Cons a as = Cons (f $ g a) $ fmap (f . g) as
+fmap f (fmap g $ Cons a as) = fmap f (Cons (g a) $ fmap g as) = Cons (f $ g a) (fmap f (fmap g as))
+-}
+
 
 -- Implement the instance and prove the laws
-instance Monad List
+instance Applicative List where
+  pure x = Cons x Nil
+  (<*>) fls xs = listFlatMap (`listMap` xs) fls
+
+{-
+pure id <*> [v_1, ..., v_i, ...] = [id] v = [id v_1, ..., id v_i, ...] = [v_1, ..., v_i, ...] = v
+
+pure f <*> pure x = [f] <*> [x] = [f x] = pure $ f x
+
+[f_1, ..., f_n] <*> pure y = [f_1 y, ..., f_n y, ...]
+pure ($ y) <*> [f_1, ..., f_n, ...] = [\x -> x y] <*> [f_1, ..., f_n, ...] = [f_1 y, ..., f_n y, ...]
+
+pure (.) <*> u <*> v <*> w = [..., \x -> u_i (v_j x), ...] <*> w = [..., u_i (v_j w_k), ...]
+u <*> (v <*> w) = [..., u_i (v_j w_k), ...]
+
+
+-}
+
+-- Implement the instance and prove the laws
+instance Monad List where
+  (>>=) Nil func = Nil
+  (>>=) as func = listFlatMap func as
+
+{-
+return a >>= h = [a] >>= h = h a --only one value flatmapped
+
+[a_1, a_2, ..., a_n, ...] >>= return
+after application of return [[a_1], [a_2], ..., [a_n], ...]
+after flattening [a_1, ..., a_n, ...]
+
+[a_1, a_2, ..., a_n, ...] >>= f >>= g
+[a_1, a_2, ..., a_n, ...] >>= (\x -> g x >>= h)
+
+Assume f a_i = [b_i_1, ..., b_i_{m_i}].
+Then both will return flatten [h b_1_1, h b_1_2, ..., h b_1_{m_1}, ..., h b_n_1, ..., h b_n_1, ..., h b_n_{m_n}, ...].
+-}
+
