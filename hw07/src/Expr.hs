308cfbf91b18842c07fd54bd08a03d20a09f7c22
Georgii Ustinov <ustinovgo@gmail.com>
2024-03-24 10:05:19 +0200

    Migrate expressions to use state

--- hw07/src/Expr.hs
+++ hw07/src/Expr.hs
@@ -1,26 +1,30 @@
 {-# LANGUAGE InstanceSigs #-}
 module Expr (Expr(..), Error(..), eval, simplify) where
 import Data.Either (fromLeft)
-import Data.Map.Strict as M ( lookup, Map, fromList )
+import StateDemo (State, execState, get, modify )
 
-data Expr a = Const a | Var String | Add (Expr a) (Expr a) | Subtract (Expr a) (Expr a) | Multiply (Expr a) (Expr a) | Divide (Expr a) (Expr a) | Power (Expr a) (Expr a) | Square (Expr a) deriving (Eq)
+data BinaryOperator = Plus | Minus | Multiply | Divide | Power deriving (Eq)
+
+data UnaryOperator = Square deriving Eq
+
+data Expr a = Const a | Var String | BinOp BinaryOperator (Expr a) (Expr a) | UnOp UnaryOperator (Expr a) deriving (Eq)
 
 instance (Show a) => Show (Expr a) where
   show :: Expr a -> String
   show (Const x) = show x
-  show (Add x y) = "(" ++ show x ++ " + " ++ show y ++ ")"
-  show (Subtract x y) = "(" ++ show x ++ " - " ++ show y ++ ")"
-  show (Multiply x y) = "(" ++ show x ++ " * " ++ show y ++ ")"
-  show (Divide x y) = "(" ++ show x ++ " / " ++ show y ++ ")"
-  show (Power x y) = show x ++ " ^ " ++ show y
-  show (Square x) = "sqrt(" ++ show x ++ ")"
+  show (BinOp Plus x y) = "(" ++ show x ++ " + " ++ show y ++ ")"
+  show (BinOp Minus x y) = "(" ++ show x ++ " - " ++ show y ++ ")"
+  show (BinOp Multiply x y) = "(" ++ show x ++ " * " ++ show y ++ ")"
+  show (BinOp Divide x y) = "(" ++ show x ++ " / " ++ show y ++ ")"
+  show (BinOp Power x y) = show x ++ " ^ " ++ show y
+  show (UnOp Square x) = "sqrt(" ++ show x ++ ")"
   show (Var name) = name
 
 instance (Num a) => Num (Expr a) where
   (+) :: Expr a -> Expr a -> Expr a
-  (+) = Add 
+  (+) = BinOp Plus
   (*) :: Expr a -> Expr a -> Expr a
-  (*) = Multiply
+  (*) = BinOp Multiply
   abs :: Expr a -> Expr a
   abs = undefined
   signum :: Expr a -> Expr a
@@ -28,8 +32,8 @@ instance (Num a) => Num (Expr a) where
   fromInteger :: Integer -> Expr a
   fromInteger = Const . fromInteger
   negate :: Expr a -> Expr a
-  negate = Multiply (Const (-1))
-  
+  negate = BinOp Multiply (Const (-1))
+
 
 data Error = DivisorIsZero | SquareRootIsNegative | PowerBaseIsNegative | VariableIsUndefined deriving Eq
 instance Show Error where
@@ -54,7 +58,7 @@ performOpWithCheck pr f a b = case pr a b of
   Just err -> Left err
 
 checkDivisorIsZero :: (Eq a, Fractional a) => Either Error a -> Either Error a -> Maybe Error
-checkDivisorIsZero _ b = case b of 
+checkDivisorIsZero _ b = case b of
   Right 0.0 -> Just DivisorIsZero
   Right _ -> Nothing
   Left err -> Just err
@@ -64,45 +68,51 @@ checkFirstArgIsNegative :: (Ord a, Num a) => Error -> Either Error a -> Either E
 checkFirstArgIsNegative err (Right x) _
   | x < 0 = Just err
   | otherwise = Nothing
-  
+
 checkFirstArgIsNegative err a _ = Just (fromLeft err a)
 
-
-getVar :: Ord k => k -> Map k b -> Either Error b
-getVar name dict = case M.lookup name dict of
+getVar :: Maybe b -> Either Error b
+getVar value = case value of
   Just x -> Right x
   _ -> Left VariableIsUndefined
 
 
-
-
-eval :: (Ord b, Floating b) => Expr b -> Map String b -> Either Error b
-eval expr dict = case expr of
-  Const val -> Right val
-  Var name -> getVar name dict
-  Add a b -> performOp (+) (eval a dict) (eval b dict)
-  Subtract a b -> performOp (-) (eval a dict) (eval b dict)
-  Multiply a b -> performOp (*) (eval a dict) (eval b dict)
-  Divide a b -> performOpWithCheck checkDivisorIsZero (/) (eval a dict) (eval b dict)
-  Power a b -> performOpWithCheck (checkFirstArgIsNegative PowerBaseIsNegative) (**) (eval a dict) (eval b dict)
-  Square a -> performOpWithCheck (checkFirstArgIsNegative SquareRootIsNegative) (**) (eval a dict) (Right 0.5)
+eval :: (Ord b, Floating b) => Expr b -> State [(String, b)] (Either Error b)
+eval (Const x) = do
+  return (Right x)
+eval (Var v) = do
+  getVar . lookup v <$> get
+eval (BinOp op x y) = do
+  f <- eval x
+  s <- eval y
+  return ((case op of 
+    Plus -> performOp (+) 
+    Minus -> performOp (-) 
+    Multiply ->  performOp (*) 
+    Divide -> performOpWithCheck checkDivisorIsZero (/)
+    Power -> performOpWithCheck (checkFirstArgIsNegative PowerBaseIsNegative) (**)) f s)
+eval (UnOp op x) = do
+  f <- eval x
+  return (case op of
+    Square -> performOpWithCheck (checkFirstArgIsNegative SquareRootIsNegative) (**) f (Right 0.5)
+    )
 
 
 simplifyAdd :: (Eq a, Fractional a) => Expr a -> Expr a -> Expr a
 simplifyAdd (Const 0.0) x = x
 simplifyAdd x (Const 0.0) = x
-simplifyAdd x y = Add x y
+simplifyAdd x y = BinOp Plus x y
 
 simplifySubtract :: (Eq a, Fractional a) => Expr a -> Expr a -> Expr a
 simplifySubtract (Const x) (Const y) 
     | x == y = Const 0.0
     | y == 0.0 = Const x
-    | otherwise = Subtract (Const x) (Const y)
+    | otherwise = BinOp Minus (Const x) (Const y)
 simplifySubtract (Var x) (Var y)
     | x == y = Const 0.0
-    | otherwise = Subtract (Var x) (Var y)
+    | otherwise = BinOp Minus (Var x) (Var y)
 simplifySubtract x (Const 0.0) = x
-simplifySubtract x y = Subtract x y
+simplifySubtract x y = BinOp Minus x y
 
 
 chooseMultiplyByZeroSimplification :: Fractional a => Expr a -> Expr a -> Expr a
@@ -112,39 +122,38 @@ chooseMultiplyByZeroSimplification initExpr expr = case expr of
   _ -> initExpr
 
 simplifyMultiply :: (Eq a, Fractional a) => Expr a -> Expr a -> Expr a
-simplifyMultiply (Const 0.0) x = chooseMultiplyByZeroSimplification (Multiply (Const 0.0) x) x
-simplifyMultiply x (Const 0.0) = chooseMultiplyByZeroSimplification (Multiply x (Const 0.0)) x
+simplifyMultiply (Const 0.0) x = chooseMultiplyByZeroSimplification (BinOp Multiply (Const 0.0) x) x
+simplifyMultiply x (Const 0.0) = chooseMultiplyByZeroSimplification (BinOp Multiply x (Const 0.0)) x
 simplifyMultiply (Const 1.0) x = x
 simplifyMultiply x (Const 1.0) = x
-simplifyMultiply x y = Multiply x y
+simplifyMultiply x y = BinOp Multiply x y
 
 simplifyDivide :: (Eq a, Fractional a) => Expr a -> Expr a -> Expr a
 simplifyDivide (Const 0.0) (Const x)
     | x /= 0 = Const 0.0
-    | otherwise = Divide (Const 0.0) (Const x)
+    | otherwise = BinOp Divide (Const 0.0) (Const x)
 simplifyDivide x (Const 1.0) = x
 simplifyDivide (Const x) (Const y)
     | x == y && x /= 0.0 = Const 1.0
-    | otherwise = Divide (Const x) (Const y)
-simplifyDivide x y = Divide x y
-
+    | otherwise = BinOp Divide (Const x) (Const y)
+simplifyDivide x y = BinOp Divide x y
 
 
 simplifyPower :: (Fractional a, Ord a) => Expr a -> Expr a -> Expr a
 simplifyPower (Const x) (Const 1.0)
     | x >= 0.0 = Const x
-    | otherwise = Power (Const x) (Const 1.0)
+    | otherwise = BinOp Power (Const x) (Const 1.0)
 simplifyPower (Const x) (Const 0.0)
      | x > 0.0 = Const 1.0
-     | otherwise = Power (Const x) (Const 0.0)
-simplifyPower x y = Power x y
+     | otherwise = BinOp Power (Const x) (Const 0.0)
+simplifyPower x y = BinOp Power x y
 
 simplify :: (Ord a, Fractional a) =>  Expr a -> Expr a
 simplify expr = case expr of
-  Add l r -> simplifyAdd (simplify l) (simplify r)
-  Subtract l r -> simplifySubtract (simplify l) (simplify r)
-  Multiply l r -> simplifyMultiply (simplify l) (simplify r)
-  Divide l r -> simplifyDivide (simplify l) (simplify r)
-  Power l r -> simplifyPower (simplify l) (simplify r)
-  Square l -> Square (simplify l)
+  BinOp Plus l r -> simplifyAdd (simplify l) (simplify r)
+  BinOp Minus l r -> simplifySubtract (simplify l) (simplify r)
+  BinOp Multiply l r -> simplifyMultiply (simplify l) (simplify r)
+  BinOp Divide l r -> simplifyDivide (simplify l) (simplify r)
+  BinOp Power l r -> simplifyPower (simplify l) (simplify r)
+  UnOp Square l -> UnOp Square (simplify l)
   _ -> expr
